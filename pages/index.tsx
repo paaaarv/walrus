87d0da4c609f48056ab094fd65020c70dbe3a826
Parvathi Kumar <parvathikumar92@gmail.com>
2024-05-09 12:59:31 -0500

    fixed build error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { Button, Group } from "@mantine/core";
-import Navigation from '../components/Navigation';
-import About from '../components/About';
+import Navigation from './components/Navigation';
+import About from './components/About';
 
 export default function IndexPage() {
   return (
