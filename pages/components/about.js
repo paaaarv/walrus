09031f479fa1f1b2ba30e4672fe3348217b217c2
Parvathi Kumar <parvathikumar92@gmail.com>
2024-05-09 15:31:10 -0500

    added google fonts to document component

--- pages/components/about.js
+++ pages/components/about.js
@@ -1,4 +1,4 @@
-import { Container, Text, Stack } from '@mantine/core';
+import { Container, Text, Stack, Typography} from '@mantine/core';
 
 
 export default function About(){
@@ -6,7 +6,7 @@ export default function About(){
     return(
         <Container p={0} style={{ height: '70vh' }}>
             <Stack pr={50} h={100} align="flex-start" justify="flex-start" gap="md">
-                <Text tt="uppercase" size="sm" fw={400}> What is Walrus?</Text>
+                <Text tt="uppercase" size="sm" fw={400}>What is Walrus?</Text>
                 <Text fw={700} size = "lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text> 
             </Stack>
         </Container>
--- theme.ts
+++ theme.ts
@@ -1,5 +1,7 @@
 import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
+  fontFamily: 'Roboto',
+  headings: {fontFamily: 'Montserrat'},
   /* Put your mantine theme override here */
 });
