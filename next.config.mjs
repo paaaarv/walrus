3cc58a789aaba97de4134fc375bc3e715c15210f
Parvathi Kumar <parvathikumar92@gmail.com>
2024-05-09 12:56:04 -0500

    to fix build error

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.cache = {
+        type: 'filesystem',
+      };
+    }
+
+    return config;
+  },
 };
+
 
 export default nextConfig;
