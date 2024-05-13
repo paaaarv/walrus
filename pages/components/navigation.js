8c46d7ccb9c9e46b2b0b93834822c348d39964d6
Parvathi Kumar <parvathikumar92@gmail.com>
2024-05-13 14:05:01 -0500

    added stylesheet

--- pages/components/navigation.js
+++ pages/components/navigation.js
@@ -1,17 +1,32 @@
-import { NavLink, Container, Flex, Group } from '@mantine/core';
+import { NavLink, Container,Grid, Flex, Text, Group,Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import classes from '../../public/css/styles.module.css';
 
 
 export default function Navigation(){
+    const [opened, { toggle } ] = useDisclosure(false);
 
     return(
         <Container h={100}>
-            <NavLink maw={100} label="WALRUS"/>
+            <Grid style={{ position: 'fixed', top: 0, left: 0, width: '100%', backgroundColor: 'white', zIndex: 100 }}>
+                <Group style = {{ width: '100%', padding: '25px 20px', flexWrap: 'nowrap' }} >
+                    <Grid.Col span={8}>
+                        <Text className={classes.navigation} size="lg">Walrus</Text>
+                    </Grid.Col>
+                   
+                    <Grid.Col span={4} style= {{ textAlign: 'right'}}>
+
+                      <Burger  size= "sm" opened = {opened} onClick = {toggle} aria-label="Toggle navigation"/>
+                    </Grid.Col>
+                </Group>
+            </Grid>
             <Flex w="80%" ml={4}>
                 <NavLink m="auto" maw={150} label="Features"/>
                 <NavLink m="auto" maw={150} label="Solutions"/>
                 <NavLink m="auto"  maw={150} label="Pricing"/>
                 <NavLink m="auto" maw={150} label="Contact Us"/>
             </Flex>
+
         </Container>
     )
 }
