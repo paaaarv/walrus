1c02eb209ce1e63e0e73f4a17fe9d9a30db4d591
Parvathi Kumar <parvathikumar92@gmail.com>
2024-05-14 09:37:45 -0500

    transition on hamburger menu nav

--- pages/components/navigation.js
+++ pages/components/navigation.js
@@ -1,4 +1,4 @@
-import { NavLink, Container,Grid, Flex, Text, Group,Burger } from '@mantine/core';
+import { NavLink, Container,Grid, Flex, Text, Group,Burger, Transition , Paper} from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import classes from '../../public/css/styles.module.css';
 
@@ -8,16 +8,42 @@ export default function Navigation(){
 
     return(
         <Container h={100}>
-            <Grid style={{ position: 'fixed', top: 0, left: 0, width: '100%', backgroundColor: 'white', zIndex: 100 }}>
+            <Grid style={{ position: 'fixed', top: 0, left: 0, width: '100%', backgroundColor: 'white', zIndex: 300 }}>
                 <Group style = {{ width: '100%', padding: '25px 20px', flexWrap: 'nowrap' }} >
                     <Grid.Col span={8}>
                         <Text className={classes.navigation} size="lg">Walrus</Text>
                     </Grid.Col>
                    
                     <Grid.Col span={4} style= {{ textAlign: 'right'}}>
-
                       <Burger className={classes.burger} size= "sm" opened = {opened} onClick = {toggle} aria-label="Toggle navigation"/>
-                    </Grid.Col>
+                      
+                       <Transition
+                            mounted={opened}
+                            transition= "pop-top-right"
+                            duration= {600}
+                            timingFunction="ease">
+                                {(styles) => (
+                            <div className={classes.navContainer} 
+                                style={{ ...styles, zIndex: 500 }}>
+                            <Paper 
+                                shadow="xs" padding="md" h="75vh"
+                                
+                            >
+                                <Group direction="column" spacing="sm">
+                                <NavLink label= "Home" href="#"></NavLink>
+                                <NavLink label= "About" href="#"></NavLink>
+                                <NavLink label= "Features" href="#"></NavLink>
+                                <NavLink label= "Pricing"  href="#"></NavLink>
+                                <NavLink label="Contact" href="#"></NavLink>
+                                </Group>
+                            </Paper>
+                            </div>
+                                )}
+                        </Transition>
+                       
+                  
+                   
+                      </Grid.Col>
                 </Group>
             </Grid>
             <Flex w="80%" ml={4}>
@@ -26,6 +52,7 @@ export default function Navigation(){
                 <NavLink m="auto"  maw={150} label="Pricing"/>
                 <NavLink m="auto" maw={150} label="Contact Us"/>
             </Flex>
+
 
         </Container>
     )
